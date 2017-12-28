5b431b9e4660d1b2cea5611aa67e855009261349
Lili <lilianamanjarres@hotmail.com>
2017-12-27 19:31:12 -0500

    modification

--- shoppingClone/js/controllers.js
+++ shoppingClone/js/controllers.js
@@ -5,6 +5,11 @@ angular.module('app.controllers', [])
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+  $scope.malls = [
+    {name:'CC. Andino', direccion:'calle 1'},
+    {name:'CC. Atlantis', direccion:'calle 1'},
+    {name:'CC. Cedritos', direccion:'calle 151'}
+  ];
 
 }])
    
@@ -45,6 +50,25 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+  $scope.shops = [
+    {name:'Studio F', local:'Piso 1 Local 120'},
+    {name:'Arturo Calle', local:'Piso 1 Local 103'},
+    {name:'Tennis', local:'Piso 3 Local 353'},
+    {name:'Tania', local:'Piso 2  Local 219'},
+    {name:'Lego', local:'Piso 3  Local 322'},
+    {name:'Totto', local:'Piso 2  Local 213'},
+    {name:'Tommy Hilfiger', local:'Piso 3  Local 334'},
+    {name:'Subway', local:'Piso 4  Local 420'},
+    {name:'Cinecolombia', local:'Piso 3  Local 311'},
+    {name:'Chevignon', local:'Piso 1  Local 102'},
+    {name:'Oma', local:'Piso 3  Local 437'},
+    {name:'Spoleto', local:'Piso 4  Local 416'},
+    {name:'Nyke', local:'Piso 1  Local 117'},
+    {name:'Bosi', local:'Piso 1  Local 104'},
+    {name:'Bella Piel', local:'Piso 2  Local 256'},
+    {name:'BBVA', local:'Piso 1  Local 109'},
+    {name:'Banco de Bogotá', local:'Piso 3  Local 356'},
+  ];
 
 }])
  
